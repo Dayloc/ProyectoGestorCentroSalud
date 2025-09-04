afa53951dd2ae61e809934d95fab2f3a659abb2a
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-09-04 16:48:52 +0000

    Explicación del repo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Navbar = () => {
+	const [role,setRole] = useState("paciente")
 
 	return (
 		<nav className="navbar navbar-light bg-light">
@@ -9,10 +11,17 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{
+						role === "paciente" ? <Link to="/demo">
+						<button className="btn btn-primary">Paciente</button>
+					</Link> : <Link to="/demo">
+						<button className="btn btn-primary">Médico</button>
+					</Link>	
+					}
+					
+				  
 				</div>
+				<button className="btn btn-primary" onClick={()=>setRole("medico")}> Cambia a Médico</button>
 			</div>
 		</nav>
 	);
---
+++ src/front/servicios/fetchs.js
@@ -0,0 +1,25 @@
+const urlBase = "https://dragonball-api.com/api/"
+
+//Get characters
+
+export const getCharacters = async (dispatch)=>{
+try {
+     const response = await fetch(`${urlBase}characters`)   
+     if(!response.ok){
+        throw new Error("Error al obtener los datos");        
+     }
+     const data =  await response.json()
+     //console.log(data.items);
+
+    dispatch({
+        type : 'saveCharacters',
+        payload: data.items
+    })
+
+     return data.items
+
+    
+} catch (error) {
+    console.log("Error",error)
+}
+}
